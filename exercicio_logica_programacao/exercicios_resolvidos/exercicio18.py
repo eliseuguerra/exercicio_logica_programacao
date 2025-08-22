9af36907eb503eaa2bc4e5c0afce84ba3eb7b5ef
Eliseu Guerra <eliseu_guerra@hotmail.com>
2025-08-22 07:42:54 -0300

    feat: add question

---
+++ exercicio_logica_programacao/exercicios_resolvidos/exercicio18.py
@@ -0,0 +1,8 @@
+vlA = int(input("Enter the first value: "))
+vlB = int(input("Enter the second value: "))
+if vlA > vlB:
+print(f"The largest value is {vlA}")
+elif vlB > vlA:
+print(f"The largest value is {vlB}")
+else:
+print("The values ​​are equal")
